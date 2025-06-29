2e12926fa51f754595cb82b4d9a76234be067f3a
Marija Stojanovic <marijastojanovic14@gmail.com>
2025-06-29 12:30:13 +0200

    Add mongo conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const databaseUrl = process.env.MONGO_URI;
+
+if (!databaseUrl) {
+  throw new Error('MONGO_URI is not set!');
+}
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(databaseUrl)],
   controllers: [AppController],
   providers: [AppService],
 })
