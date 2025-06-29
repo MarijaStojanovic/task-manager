9cda086addb74b2a93efba10e46918df96ec8255
Marija Stojanovic <marijastojanovic14@gmail.com>
2025-06-29 13:18:50 +0200

    Setup config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-const databaseUrl = process.env.MONGO_URI;
-
-if (!databaseUrl) {
-  throw new Error('MONGO_URI is not set!');
-}
 @Module({
-  imports: [MongooseModule.forRoot(databaseUrl), TaskModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const databaseUrl = configService.get<string>('MONGO_URI');
+        if (!databaseUrl) {
+          throw new Error('MONGO_URI is not set!');
+        }
+        return { uri: databaseUrl };
+      },
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
